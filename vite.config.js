0ad12762f74d320a1942b61a8b59e68974882cd8
Puneeth Reddy <gondipuneethreddy@gmail.com>
2026-03-13 16:55:05 +0530

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for proper routing on Vercel
+  base: './', // Fixes white screen on Vercel
 });
